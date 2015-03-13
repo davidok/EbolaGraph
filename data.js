80c58fbdf767ca5a867fa50de7a343d0c418c0ff
David Ok <davidok@gmail.com>
2015-03-13 15:25:52 -0400

    Loaded Ebola cases and deaths information to Data file.

---
+++ data.js
@@ -0,0 +1,19 @@
+var casesData = [
+	{date: "2014-08-29", cases: 3052},
+	{date: "2014-09-28", cases: 7157},
+	{date: "2014-10-31", cases: 13567},
+	{date: "2014-11-26", cases: 15935},
+	{date: "2014-12-31", cases: 20206},
+	{date: "2015-01-25", cases: 22092},
+	{date: "2015-02-22", cases: 23729}
+]
+
+var deathsData2 = [
+	{date: "2014-08-29", deaths: 1546},
+	{date: "2014-09-28", deaths: 3330},
+	{date: "2014-10-31", deaths: 4951},
+	{date: "2014-11-26", deaths: 5689},
+	{date: "2014-12-31", deaths: 7904},
+	{date: "2015-01-25", deaths: 8810},
+	{date: "2015-02-22", deaths: 9604}
+]
